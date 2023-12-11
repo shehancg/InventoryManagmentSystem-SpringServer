43380aa940be7c55070cf3936a95bd4bb06fe34f
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-12-12 00:41:03 +0530

    Created User Model and did a DB Migration

--- src/main/java/com/exe/inventorymsystemserver/InventoryMSystemServerApplication.java
+++ src/main/java/com/exe/inventorymsystemserver/InventoryMSystemServerApplication.java
@@ -1,13 +1,15 @@
 package com.exe.inventorymsystemserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryMSystemServerApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(InventoryMSystemServerApplication.class);
 	public static void main(String[] args) {
-
+		logger.info("Your log message");
 		System.out.println("HELLO");
 		SpringApplication.run(InventoryMSystemServerApplication.class, args);
 	}
--- src/main/java/com/exe/inventorymsystemserver/Model/User.java
+++ src/main/java/com/exe/inventorymsystemserver/Model/User.java
@@ -1,11 +1,39 @@
 package com.exe.inventorymsystemserver.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "users")
+@Data
 public class User {
 
-    private 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "user_name", unique = false, nullable = false)
+    private String userName;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "is_active", nullable = true)
+    private boolean isActive;
+
+    @Column(name = "user_type_id", nullable = true)
+    private Long userTypeId;
+
+    public User(Long userId, String userName, String password, boolean isActive, Long userTypeId) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.isActive = isActive;
+        this.userTypeId = userTypeId;
+    }
+
+    public User() {
+
+    }
 }
