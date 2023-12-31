8fee2ab54b9fde6c3dca8d4795aab5251afc4b98
Shehan Gunawardena <64646307+shehancg@users.noreply.github.com>
2023-12-31 17:01:33 +0530

    Added CORS & start bin file

--- src/main/java/com/exe/inventorymsystemserver/InventoryMSystemServerApplication.java
+++ src/main/java/com/exe/inventorymsystemserver/InventoryMSystemServerApplication.java
@@ -2,12 +2,28 @@ package com.exe.inventorymsystemserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class InventoryMSystemServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryMSystemServerApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:3000")
+						.allowedMethods("GET", "POST", "PUT", "DELETE")
+						.allowedHeaders("*");
+			}
+		};
 	}
 
 }
